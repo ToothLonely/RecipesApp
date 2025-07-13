22afdfde3b2399b10f6549773bde646ccf629660
ToothLonely <arsen.shirinov2004@gmail.com>
2025-07-14 01:03:41 +0300

    RA-09 deleted unecessary import

--- app/src/main/java/com/example/recipesapp/CategoriesListAdapter.kt
+++ app/src/main/java/com/example/recipesapp/CategoriesListAdapter.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recipesapp.databinding.ItemCategoryBinding
-import java.io.InputStream
 
 class CategoriesListAdapter(private val dataSet: List<Category>) :
     RecyclerView.Adapter<CategoriesListAdapter.ViewHolder>() {
